80ec075f4a97d28f029a5993628357237006db16
Blair Zong <blair.zong167103@gmail.com>
2025-08-22 03:57:09 +1000

    completed rational operator overloads

--- src/objects/rational.cpp
+++ src/objects/rational.cpp
@@ -1,54 +1,71 @@
 #include "rational.h"
 
 // Default constructor
-Rational::Rational() : Rational(0) {}
+Rational::Rational()
+: Rational(0) {}
 
-Rational::Rational(long long numerator) : Rational(numerator, 1) {}
+Rational::Rational(ll numerator)
+: Rational(numerator, 1) {}
 
-Rational::Rational(long long numerator, long long denominator) : numerator_(numerator), denominator_(denominator) {
-    if (denominator_ == 0)
-        throw std::domain_error{"Division by 0 error!"};
-    simplify();
+Rational::Rational(ll numerator, ll denominator)
+: numerator_(numerator)
+, denominator_(denominator) {
+	if (denominator_ == 0)
+		throw std::domain_error{"Division by 0 error!"};
+	simplify();
 }
 
-// Given an potentially decimal number, multiply it by the smallest power of 10 which will make it an integer, and set that power of 10 as the denominator.
+// Given an potentially decimal number, multiply it by the smallest power of 10
+// which will make it an integer, and set that power of 10 as the denominator.
 //  e.g 3.14 = 314/100, since there are two digits after the decimal place.
 Rational::Rational(const std::string& expr) {
-    const auto decimal_places = expr.find('.') == std::string::npos ? 0 : expr.length() - 1 - expr.find('.');
-    denominator_ = (Rational{10}^decimal_places).numerator_;
-    numerator_ = static_cast<long long>(std::stold(expr) * denominator_);
-    simplify();
+	const auto decimal_places =
+	   expr.find('.') == std::string::npos ? 0 : expr.length() - 1 - expr.find('.');
+	denominator_ = (Rational{10} ^ decimal_places).numerator_;
+	numerator_ = static_cast<ll>(std::stold(expr) * denominator_);
+	simplify();
 }
 
 auto operator^(const Rational base, size_t power) -> Rational {
-    auto current = base;
-    auto result = Rational{1};
-    while (power) {
-        if (power & 1) result *= current;
-        current *= current;
-        power >>= 1;
-    }
-    return result;
+	auto current = base;
+	auto result = Rational{1};
+	while (power) {
+		if (power & 1)
+			result *= current;
+		current *= current;
+		power >>= 1;
+	}
+	return result;
+}
+
+auto operator<=>(const Rational& lhs, const Rational& rhs) -> std::strong_ordering {
+	if (lhs.numerator_ >= 0 && rhs.numerator_ < 0)
+		return std::strong_ordering::greater;
+	const auto ratio = lhs / rhs;
+	return ratio.numerator_ <=> ratio.denominator_;
 }
 
 auto operator<<(std::ostream& os, const Rational& rational) -> std::ostream& {
-    os << rational.numerator_;
-    if (rational.denominator_ != 1) os << "/" << rational.denominator_;
-    return os;
+	os << rational.numerator_;
+	if (rational.denominator_ != 1)
+		os << "/" << rational.denominator_;
+	return os;
 }
 
-auto Rational::gcd(unsigned long long a, unsigned long long b) -> long long {
-    if (a < b) std::swap(a, b);
-    return b == 0 ? a : gcd(b, a % b);
+auto Rational::gcd(ull a, ull b) -> ll {
+	if (a < b)
+		std::swap(a, b);
+	return b == 0 ? static_cast<ll>(a) : gcd(b, a % b);
 }
 
-// Simplify the rational number by dividing numerator and denominator by their greatest common divisor
+// Simplify the rational number by dividing numerator and denominator by their
+// greatest common divisor
 auto Rational::simplify() -> void {
-    if (denominator_ < 0) {
-        numerator_ *= -1;
-        denominator_ *= -1;
-    }
-    const auto gcd = Rational::gcd(abs(numerator_), denominator_);
-    numerator_ /= gcd;
-    denominator_ /= gcd;
+	if (denominator_ < 0) {
+		numerator_ *= -1;
+		denominator_ *= -1;
+	}
+	const auto gcd = Rational::gcd(static_cast<ull>(abs(numerator_)), static_cast<ull>(denominator_));
+	numerator_ /= gcd;
+	denominator_ /= gcd;
 }
